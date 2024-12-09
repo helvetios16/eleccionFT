bacc78f29314c3c032335c266af0c5962cffacbe
helvetios16 <smendozafernandez437@gmail.com>
2024-12-09 16:23:24 -0500

    limpieza

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -1,8 +1,7 @@
 function toggleMenu(button) {
-    const menu = document.getElementById(button.getAttribute('aria-controls'));
-    const isExpanded = button.getAttribute('aria-expanded') === 'true';
+  const menu = document.getElementById(button.getAttribute("aria-controls"));
+  const isExpanded = button.getAttribute("aria-expanded") === "true";
 
-    // Cambiar el estado del botón y del menú
-    button.setAttribute('aria-expanded', !isExpanded);
-    menu.classList.toggle('hidden', isExpanded);
-  }
+  button.setAttribute("aria-expanded", !isExpanded);
+  menu.classList.toggle("hidden", isExpanded);
+}
